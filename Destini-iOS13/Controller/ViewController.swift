afecde25179a2a2261db557d3839d00161b69d1a
Mark <mark.arias7@gmail.com>
2023-09-08 19:11:27 -0500

    base line changes. created basic model for a story. basic logic for story nav.

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -8,6 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var userStoryPosition = 0  // user starts a story point 0, the beggining.
+    
+    let stories = [
+        Story(title: "You see a fork in the road.",
+                           choice1: "Take a left.",
+                           choice2: "Take a right."
+        ),
+        Story(title: "You see a tiger.",
+                           choice1: "Shout for help.",
+                           choice2: "Play dead."
+        ),
+        Story(title: "You find a treasure chest.",
+                           choice1: "Open it.",
+                           choice2: "Check for traps."
+        )
+    ]
 
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
@@ -15,8 +32,31 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        choice1Button.tag = 1
+        choice2Button.tag = 2
+        
+        storyLabel.text = stories[0].title
+        choice1Button.setTitle(stories[0].choice1, for: .normal)
+        choice2Button.setTitle(stories[0].choice2, for: .normal)
 
     }
+    
+    @IBAction func choiceMade(_ sender: UIButton) {
+        if sender.tag == 1 {    // user chose left choice
+            userStoryPosition = 1
+            updateUI(for: userStoryPosition)
+        } else if sender.tag == 2 {     // user chose right choice
+            userStoryPosition = 2
+            updateUI(for: userStoryPosition)
+        }
+    }
+    
+    func updateUI(for userLocation: Int) {
+        storyLabel.text = stories[userLocation].title
+        choice1Button.setTitle(stories[userLocation].choice1, for: .normal)
+        choice2Button.setTitle(stories[userLocation].choice2, for: .normal)
+    }
+    
 
 
 }
